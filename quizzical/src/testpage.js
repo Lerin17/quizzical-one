d5927ba8f040f888fcd1295959358bf2bd1b9870
Lerin17 <lerinowoade17@gmail.com>
2022-05-07 14:56:02 -0500

    api call and render #2

--- quizzical/src/testpage.js
+++ quizzical/src/testpage.js
@@ -1,12 +1,43 @@
 import React from "react";
 
 export default  function Testpage(){
+const [isdata, setisdata] = React.useState();
+const [testdata, settestdata] = React.useState();
+let question
+
+
+
 
     React.useEffect(() => {
         fetch('https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple').then(Response => Response.json() )
-        .then(data => console.log(data))    
+        .then(data => settestdata(data.results)
+        )  
 }, []);
 
-    
+
+if(testdata){
+    console.log(testdata)
+    question =   testdata.map(item => 
+        <div key = {testdata.indexOf(item)}>
+            <h1>{item.question} </h1>
+        </div>
+    )
+}
+
+
+if(testdata){
+    return(
+        <div>
+            {question}
+        </div>
+        )   
+}else{
+    return (
+        <h1>
+         questions loading...
+        </h1>
+    )
+}
+  
 }
 
